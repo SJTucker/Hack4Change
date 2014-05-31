d09f9a9bc96d5b9989ab9d5c8320507494e296a1
Nat Webb <natrwebb@gmail.com>
2014-05-31 15:10:59 -0500

    Page auto-loads public art; this is an interim step towards loading any desired data

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,9 +5,11 @@
   $(document).ready(initialize);
 
   var map;
+  //var markers = [];
 
   function initialize(){
     initMap(36.1667, -86.7833, 12);
+    getArt();
   }
 
   function initMap(lat, lng, zoom){
@@ -15,10 +17,25 @@
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
-  //function addMarker(location){
-  //  var position = new google.maps.LatLng(location.lat, location.lng);
-  //  var marker = new google.maps.Marker({map:map, position:position, title:location.address});
-  //  markers.push(marker);
-  //}
+  function getArt(){
+    var url = "http://data.nashville.gov/resource/dqkw-tj5j.json";
+    $.getJSON(url, pinData);
+  }
+
+  function pinData(data){
+    console.log(data);
+    for(var i=0; i<data.length; i++){
+      var title = data[i].title;
+      var lat = data[i].latitude;
+      var lng = data[i].longitude;
+      addMarker(lat, lng, title);
+    }
+  }
+
+  function addMarker(lat, lng, title){
+    var position = new google.maps.LatLng(lat, lng, title);
+    var marker = new google.maps.Marker({map:map, position:position, title:title});
+    //markers.push(marker);
+  }
 
 })();

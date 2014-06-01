eb455840f213ed25ddae79b43f1bb62fa8a97673
Sam Tucker <samueljeffersontucker@gmail.com>
2014-06-01 11:30:04 -0500

    non-functioning stripper

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0'
+gem 'activerecord'
 
 gem 'spring'
 
--- oldkmlstripper.rb
+++ oldkmlstripper.rb
@@ -1,5 +1,6 @@
-
+require_relative 'app/models/medianincome'
+@incomes = Medianincome.incomes
 
 output = File.read('districts.kml')
 results = output.scan(/DISTRICT<\/span>:<\/strong> <span class="atr-value">\d+/)
-puts results
+puts @incomes
